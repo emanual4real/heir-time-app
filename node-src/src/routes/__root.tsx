bed48930300d91e8ccdb8be2ac5985bfdfa38337
Manny <emanual4real@hotmail.com>
2024-04-20 23:08:07 +0000

    render tanstack dev tools in dev mode only

--- node-src/src/routes/__root.tsx
+++ node-src/src/routes/__root.tsx
@@ -1,9 +1,10 @@
 import { rootRouteWithContext, Outlet } from '@tanstack/react-router';
-import { TanStackRouterDevtools } from '@tanstack/router-devtools';
 import { Container } from '@mui/material';
 import { NavBar } from '@ui/components';
 import { User } from '@ui/types';
 import { useGetSelfQuery } from '@ui/services';
+import { Suspense } from 'react';
+import React from 'react';
 
 interface RouteContext {
   user: User | null;
@@ -13,13 +14,27 @@ interface RouteContext {
 const RootComponent = () => {
   const { data: user, isSuccess } = useGetSelfQuery();
 
+  const TanStackRouterDevtools =
+    import.meta.env.MODE !== 'dev'
+      ? () => null // Render nothing in production
+      : React.lazy(() =>
+          // Lazy load in development
+          import('@tanstack/router-devtools').then((res) => ({
+            default: res.TanStackRouterDevtools
+            // For Embedded Mode
+            // default: res.TanStackRouterDevtoolsPanel
+          }))
+        );
+
   return (
     <>
       <NavBar loggedIn={isSuccess} isAdmin={user?.isAdmin} />
       <Container>
         <Outlet />
       </Container>
-      <TanStackRouterDevtools />
+      <Suspense>
+        <TanStackRouterDevtools />
+      </Suspense>
     </>
   );
 };
